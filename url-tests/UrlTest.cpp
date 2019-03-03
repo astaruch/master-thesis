d2829b4c8907f48797d75a59524e344d5f261188
Andrej Staruch <staruch.andrej@gmail.com>
2019-03-03 18:31:59 +0100

    Added simple test for special chars. Closes #9

--- url-tests/UrlTest.cpp
+++ url-tests/UrlTest.cpp
@@ -6,25 +6,24 @@
 #include "UrlTest.h"
 
 UrlTest::UrlTest()
-        :m_test_flags(Test::None)
-{
+        : m_test_flags(Test::None) {
 }
 
-void UrlTest::SetUrl(const std::string& url)
-{
+void
+UrlTest::SetUrl(const std::string &url) {
     // TODO: add some logger
     std::cout << "Setting up the URL: " << url << "\n";
     m_raw_url = url;
     try {
         m_url = Poco::URI(url);
     }
-    catch (const Poco::Exception& ex) {
+    catch (const Poco::Exception &ex) {
         std::cerr << ex.what() << ex.message() << "\n";
     }
 }
 
-int UrlTest::PerformtTests()
-{
+int
+UrlTest::PerformtTests() {
     std::cout << "Executing tests...\n";
     int result = 0;
     if (m_test_flags & Test::Length) {
@@ -42,17 +41,17 @@ int UrlTest::PerformtTests()
     return result;
 }
 
-void UrlTest::AddTestLength(int length)
-{
+void
+UrlTest::AddTestLength(int length) {
     std::cout << "Adding test for length (" << length << ").\n";
     m_test_flags |= Test::Length;
     m_test_url_length = length;
 }
 
-int UrlTest::TestLength()
-{
+int
+UrlTest::TestLength() {
     std::cout << "Testing URL length... ";
-    if (m_raw_url.size()>m_test_url_length) {
+    if (m_raw_url.size() > m_test_url_length) {
         std::cout << "FAIL\n";
         return 1;
     }
@@ -60,21 +59,21 @@ int UrlTest::TestLength()
     return 0;
 }
 
-void UrlTest::AddTestDepth(int depth)
-{
+void
+UrlTest::AddTestDepth(int depth) {
     std::cout << "Adding test for depth (" << depth << ").\n";
     m_test_flags |= Test::Depth;
     m_test_url_depth = depth;
 }
 
-int UrlTest::TestDepth()
-{
+int
+UrlTest::TestDepth() {
     std::cout << "Testing URL depth... ";
-    const auto& host = m_url.getHost();
-    auto dots = std::count_if(host.begin(), host.end(), [](const char& c) {
-        return c=='.';
+    const auto &host = m_url.getHost();
+    auto dots = std::count_if(host.begin(), host.end(), [](const char &c) {
+        return c == '.';
     });
-    if (dots>m_test_url_depth) {
+    if (dots > m_test_url_depth) {
         std::cout << "FAIL\n";
         return 1;
     }
@@ -82,26 +81,44 @@ int UrlTest::TestDepth()
     return 0;
 }
 
-void UrlTest::AddTestSpecialChars()
-{
+void
+UrlTest::AddTestSpecialChars() {
     std::cout << "Adding test for special characters.\n";
     m_test_flags |= Test::SpecialChars;
 }
 
-int UrlTest::TestSpecialChars()
-{
+int
+UrlTest::TestSpecialChars() {
     int result = 0;
-    std::cout << "Testing special chars in URL... ";
+    std::cout << "Testing special chars in URL... \n";
+
+    std::cout << "@ (user@password) ... ";
     if (!m_url.getUserInfo().empty()) {
         // @
         result += 1;
+        std::cout << "YES\n";
+    } else {
+        std::cout << "NO\n";
+    }
+
+    // TODO: determine correct values for these special chars
+    // TODO: test maybe only www-google-com.info or something like that with special chars
+    std::vector<char> special_chars{'@', '/', '?', '.', '=', '-', '_'};
+    for (const auto &c: special_chars) {
+        std::cout << c << " ... ";
+        if (std::count(m_raw_url.begin(), m_raw_url.end(), c) > 0) {
+            result += 1; // TODO: change to something meaningful
+            std::cout << "YES\n";
+        } else {
+            std::cout << "NO\n";
+        }
     }
     std::cout << (result ? "FAIL" : "PASS") << std::endl;
     return result;
 }
 
-void UrlTest::AddTestKeywords(const std::string& keywords)
-{
+void
+UrlTest::AddTestKeywords(const std::string &keywords) {
     std::cout << "Adding test for keywords.\n";
     m_test_flags |= Test::Keywords;
 
@@ -109,12 +126,12 @@ void UrlTest::AddTestKeywords(const std::string& keywords)
     m_test_keywords = std::vector<std::string>(tokens.begin(), tokens.end());
 }
 
-int UrlTest::TestKeywords()
-{
+int
+UrlTest::TestKeywords() {
     std::cout << "Testing keywords in URL... ";
     int result = 0;
-    for (const auto& keyword: m_test_keywords) {
-        if (m_raw_url.find(keyword)!=std::string::npos) {
+    for (const auto &keyword: m_test_keywords) {
+        if (m_raw_url.find(keyword) != std::string::npos) {
             result += 1;
         }
     }
