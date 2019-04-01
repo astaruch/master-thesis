48623e7f8eb35ab3cc9771d1a80cba7f93cfacee
Andrej Staruch <staruch.andrej@gmail.com>
2019-04-01 07:37:21 +0200

    Add socket client application
    This is simple testing client application to test socket connection
    between phishing application and this client. Later will be refactored
    to send URLs and other options.

---
+++ socket-client/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <cstring>
+#include <limits>
+#include <arpa/inet.h>
+#include <zconf.h>
+
+int main(int argc, const char** argv)
+{
+    std::string strSimpleUsage = std::string(argv[0]) + " <PORT>\n";
+    if (argc < 2)
+    {
+        std::cerr << "Please enter port where is a phishing application listening...\n\n" << strSimpleUsage;
+        return -1;
+    }
+
+    uint16_t nPort = 0;
+    try
+    {
+        auto iPort = std::stoi(argv[1]);
+        if (0 <= iPort && 65535 >= iPort)
+        {
+            nPort = static_cast<uint16_t>(iPort);
+        } else
+        {
+            throw std::invalid_argument("Entered port is not in range 0-65535.");
+        }
+    } catch (std::invalid_argument& ex)
+    {
+        std::cerr << ex.what() << std::endl << strSimpleUsage;
+        return -1;
+    }
+
+    std::cout << "Client socket application. Connect to localhost:" << nPort << std::endl;
+
+    struct sockaddr_in address{};
+    struct sockaddr_in server_address{};
+    int sock = 0;
+    const char* hello = "Hello from client";
+    char buffer[1024] = {0};
+
+    std::cout << "socket()\n";
+    if (-1 == (sock = socket(AF_INET, SOCK_STREAM, 0)))
+    {
+        std::cerr << "Socket creation error. Exiting...\n";
+        return -1;
+    }
+
+    memset(&server_address, '0', sizeof(server_address));
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(nPort);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (-1 == inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr))
+    {
+        std::cerr << "Invalid server address\n";
+        return -1;
+    }
+
+    std::cout << "connect().\n";
+    if (-1 == connect(sock, (struct sockaddr *)&server_address, sizeof(server_address)))
+    {
+        std::cerr << "connect failed\n";
+        return -1;
+    }
+
+    std::cout << "send()\n";
+    if (-1 == send(sock, hello, strlen(hello), 0))
+    {
+        std::cerr << "send failed\n";
+        return -1;
+    }
+
+    std::cout << "read()\n";
+    if (0 >= read(sock, buffer, 1024))
+    {
+        std::cerr << "read failed\n";
+        return -1;
+    }
+    std::cout << buffer << std::endl;
+
+    std::cout << "close()\n";
+    if (-1 == close(sock))
+    {
+        std::cerr << "close failed\n";
+        return -1;
+    }
+    return 0;
+}
