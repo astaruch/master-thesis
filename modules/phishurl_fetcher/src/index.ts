72fa689ec815453387296479e88de6f36899c5f6
Andrej Staruch <staruch.andrej@gmail.com>
2019-04-22 04:01:34 +0200

    Switch from console.log to pino-logger

--- modules/phishurl_fetcher/src/index.ts
+++ modules/phishurl_fetcher/src/index.ts
@@ -1,27 +1,35 @@
 import 'reflect-metadata'
 import { createConnection } from 'typeorm'
+import pino, { Logger } from 'pino'
 import { Phishtank } from './entity/Phishtank'
 
+const logger: Logger = pino({})
 
-createConnection().then(async connection => {
-  console.log('Inserting a new record into the database...')
+const run = (): void => {
+  createConnection().then(async connection => {
+    logger.info('Inserting a new record into the database...')
 
-  const phishtankRecord = new Phishtank()
-  phishtankRecord.phishId = 6014538
-  phishtankRecord.url = 'https://outletdeprimaveira.com/index.php?produto=489466851'
-  phishtankRecord.phishDetailUrl = 'http://www.phishtank.com/phish_detail.php?phish_id=6014538'
-  phishtankRecord.submssionTime = '2019-04-17T20:36:17+00:00'
-  phishtankRecord.verificationTime = '2019-04-17T20:37:05+00:00'
-  phishtankRecord.online = true
-  phishtankRecord.target = 'Other'
+    const phishtankRecord = new Phishtank()
+    phishtankRecord.phishId = 6014538
+    phishtankRecord.url = 'https://outletdeprimaveira.com/index.php?produto=489466851'
+    phishtankRecord.phishDetailUrl = 'http://www.phishtank.com/phish_detail.php?phish_id=6014538'
+    phishtankRecord.submssionTime = '2019-04-17T20:36:17+00:00'
+    phishtankRecord.verificationTime = '2019-04-17T20:37:05+00:00'
+    phishtankRecord.online = true
+    phishtankRecord.target = 'Other'
 
 
-  await connection.manager.save(phishtankRecord)
-  console.log(`Saved a new user with id: ${phishtankRecord.id}`)
+    await connection.manager.save(phishtankRecord)
+    logger.info(`Saved a new user with id: ${phishtankRecord.id}`)
 
-  console.log('Loading users from the database...')
-  const records = await connection.manager.find(Phishtank)
-  console.log('Loaded users: ', records)
+    logger.info('Loading users from the database...')
+    const records = await connection.manager.find(Phishtank)
+    logger.info('Loaded users: ', records)
 
-  console.log('Here you can setup and run express/koa/any other framework.')
-}).catch(error => console.log(error))
+    logger.info('Here you can setup and run express/koa/any other framework.')
+  }).catch(error => logger.info(error))
+}
+
+if (require.main === module) {
+  run()
+}
