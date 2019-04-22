7566f3d585102df6e6319f4e707f94a2dc960a69
Andrej Staruch <staruch.andrej@gmail.com>
2019-04-22 20:38:58 +0200

    Parse csv

--- modules/phishurl_fetcher/src/index.ts
+++ modules/phishurl_fetcher/src/index.ts
@@ -1,11 +1,59 @@
 import 'reflect-metadata'
+import * as fs from 'fs'
+import { promisify } from 'util'
+import * as path from 'path'
+import { userInfo } from 'os'
 import { createConnection } from 'typeorm'
 import pino, { Logger } from 'pino'
+import parse from 'csv-parse/lib/sync'
 import { Phishtank } from './entity/Phishtank'
+
+// eslint-disable-next-line camelcase
+interface PhishtankRecordType {
+  // eslint-disable-next-line camelcase
+  phish_id: string
+  url: string
+  // eslint-disable-next-line camelcase
+  phish_detail_url: string
+  // eslint-disable-next-line camelcase
+  submission_time: string
+  verified: string
+  // eslint-disable-next-line camelcase
+  verification_time: string
+  online: string
+  target: string
+}
 
 const logger: Logger = pino({})
 
+const readdirAsync = promisify(fs.readdir)
+
+const getFilenames = async (inputDir: string): Promise<string[]> => {
+  try {
+    return await readdirAsync(inputDir)
+  } catch (err) {
+    logger.error(`Entered input directory doesn't exists: ${inputDir}`)
+    return []
+  }
+}
+
+
 const run = (): void => {
+  const dir = path.join(__dirname, '../../../../master-thesis-phishtank-data/data/csv')
+  getFilenames(dir).then(files => {
+    files.forEach(file => {
+      // process 1 csv file and insert into database
+      const fullpath = path.join(dir, file)
+      logger.info(fullpath)
+      const csvContent = fs.readFileSync(fullpath)
+      const records: PhishtankRecordType[] = parse(csvContent.toString('utf8'), {
+        columns: true,
+        delimiter: ',',
+      })
+
+      console.log(records)
+    })
+  })
   createConnection().then(async connection => {
     logger.info('Inserting a new record into the database...')
 
