fe9896e8939808e0dd80c74a59e4fc7e94cb1b51
Andrej Staruch <staruch.andrej@gmail.com>
2019-10-04 20:45:19 +0200

    Add small working example with pqxx connection

--- v2/service/src/main.cpp
+++ v2/service/src/main.cpp
@@ -3,8 +3,22 @@
 #include <string>
 
 #include <spdlog/spdlog.h>
+#include <pqxx/pqxx>
 
 int main() {
     spdlog::info("logger loaded");
+    spdlog::info("connecting to database");
+
+    std::string conn_string("postgresql://postgres:password@172.17.0.3/urls");
+
+    pqxx::connection c(conn_string);
+    pqxx::work txn(c);
+
+    pqxx::row r = txn.exec1(
+        "SELECT COUNT(*) FROM phishtank"
+    );
+
+    int pt_count = r[0].as<int>();
+    spdlog::info("There are {} records in phishtank table.", pt_count);
     return 0;
 }
