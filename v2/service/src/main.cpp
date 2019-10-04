8e3671bc923e7948a3aa7b5317c3b7c166d996b8
Andrej Staruch <staruch.andrej@gmail.com>
2019-10-04 22:58:15 +0200

    Add Poco library and small example

--- v2/service/src/main.cpp
+++ v2/service/src/main.cpp
@@ -4,6 +4,11 @@
 
 #include <spdlog/spdlog.h>
 #include <pqxx/pqxx>
+#include <Poco/URI.h>
+#include <Poco/Exception.h>
+
+
+using std::string;
 
 int main() {
     spdlog::info("logger loaded");
@@ -20,5 +25,26 @@ int main() {
 
     int pt_count = r[0].as<int>();
     spdlog::info("There are {} records in phishtank table.", pt_count);
+
+    r = txn.exec1(
+        "SELECT * FROM phishtank"
+        "  LIMIT 1"
+    );
+
+    auto url_from_db = r[1].as<string>();
+    spdlog::info("URL: {}", url_from_db);
+
+    Poco::URI url_;
+    try {
+        spdlog::info("Parsing URL");
+        url_ = Poco::URI(url_from_db);
+    }
+    catch (const Poco::Exception& ex) {
+        spdlog::error("Can't parse URL: {} - {}", ex.what(), ex.message());
+        return 1;
+    }
+
+    spdlog::info("Host: {}", url_.getHost());
+
     return 0;
 }
