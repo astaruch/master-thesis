a5058c1c116d3bc928939be5762bc75fe98154d7
Andrej Staruch <staruch.andrej@gmail.com>
2019-10-26 15:18:07 +0000

    Add failing tests for feature IP Address

--- v2/service/test/main.cpp
+++ v2/service/test/main.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
 #include "gtest/gtest.h"
 
-TEST(One, EqualsOne)
+#include "features/ip_address.h"
+
+TEST(HostIPAddress, NoIPAddress)
 {
-    EXPECT_EQ(1, 1);
+    feature::ip_address f;
+    f.set_url("http://google.com");
+    EXPECT_EQ(0., f.compute_value());
+}
+
+TEST(IPAddress, ClassicIPAddress)
+{
+    feature::ip_address f;
+    f.set_url("http://138.197.136.185/bnc/National\%20Bank\%20Online.html");
+    EXPECT_EQ(1., f.compute_value());
+}
+
+TEST(HostIPAddress, Octal)
+{
+    feature::ip_address f;
+    f.set_url("https://0254.0331.027.0356");
+    EXPECT_EQ(0., f.compute_value());
+}
+
+TEST(IPAddress, Decimal)
+{
+    feature::ip_address f;
+    f.set_url("https://2899908590");
+    EXPECT_EQ(1., f.compute_value());
+}
+
+TEST(HostIPAddress, Hexadecimal)
+{
+    feature::ip_address f;
+    f.set_url("http://0x308f647/-credit-agricole-france/");
+    EXPECT_EQ(0., f.compute_value());
 }
 
 int main(int argc, char **argv) {
