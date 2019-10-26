d91956e1a687a7b742787a6956ec586687a271e1
Andrej Staruch <staruch.andrej@gmail.com>
2019-10-26 15:36:09 +0000

    Fix typos in test names

--- v2/service/test/main.cpp
+++ v2/service/test/main.cpp
@@ -10,28 +10,28 @@ TEST(HostIPAddress, NoIPAddress)
     EXPECT_EQ(0., f.compute_value());
 }
 
-TEST(IPAddress, ClassicIPAddress)
+TEST(HostIPAddress, ClassicDotForm)
 {
     feature::ip_address f;
     f.set_url("http://138.197.136.185/bnc/National\%20Bank\%20Online.html");
     EXPECT_EQ(1., f.compute_value());
 }
 
-TEST(HostIPAddress, Octal)
+TEST(HostIPAddress, OctalForm)
 {
     feature::ip_address f;
     f.set_url("https://0254.0331.027.0356");
     EXPECT_EQ(0., f.compute_value());
 }
 
-TEST(IPAddress, Decimal)
+TEST(HostIPAddress, DecimalForm)
 {
     feature::ip_address f;
     f.set_url("https://2899908590");
     EXPECT_EQ(1., f.compute_value());
 }
 
-TEST(HostIPAddress, Hexadecimal)
+TEST(HostIPAddress, HexadecimalForm)
 {
     feature::ip_address f;
     f.set_url("http://0x308f647/-credit-agricole-france/");
