338bc3fa9165ed29dc1cbf780e8d03f461cf9f94
Andrej Staruch <staruch.andrej@gmail.com>
2019-10-28 14:52:17 +0100

    Correct length for path

--- v2/service/src/features/path_length.cpp
+++ v2/service/src/features/path_length.cpp
@@ -7,7 +7,8 @@ double path_length::compute_value()
     if (!_url_is_ok) {
         return _value;
     }
-    auto len = _url_obj.getPath().length() > _max_length ? _max_length : _url_obj.getPath().length();
+    auto len = std::min(_url_obj.getPath().length(), _max_length);
+    len = std::max(len, _min_length);
     return static_cast<double>(len - _min_length) / static_cast<double>(_max_length - _min_length);
 }
 
