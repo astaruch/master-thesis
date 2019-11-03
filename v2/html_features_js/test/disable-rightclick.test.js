a90cce811f4f91c9d61730cb7c716aa36c2a95d1
Andrej Staruch <staruch.andrej@gmail.com>
2019-11-03 12:49:11 +0100

    Add test disable rightclick

---
+++ v2/html_features_js/test/disable-rightclick.test.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const Page = require('../src/page')
+const assert = require('assert')
+const { JSDOM } = require('jsdom')
+
+
+describe('FeatureTests', function() {
+  describe('DisableRightclick', function() {
+    const url = 'https://google.com'
+    const DOM1 = `<body oncontextmenu="return false;">`
+
+    const DOM2 = `
+<div>
+  <a href="http://phishing.com"
+      onmouseover="this.src='map_location_revealed.gif';"
+      onmouseout="this.src='map.gif';"/>Figures for February’s
+</div>
+`
+    const page = new Page(url)
+
+    it('disabled rightlick should return 1', () => {
+      const DOM = new JSDOM(DOM1)
+      const forms = page.featureDisableRightclick(DOM)
+      assert.strictEqual(forms, 1)
+    })
+    it('enabled rightlick should return 0', () => {
+      const DOM = new JSDOM(DOM2)
+      const forms = page.featureDisableRightclick(DOM)
+      assert.strictEqual(forms, 0)
+    })
+  })
+})
