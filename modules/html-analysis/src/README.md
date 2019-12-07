1e01fe81c708077f8f9a2267baa463980d318dce
Andrej Staruch <staruch.andrej@gmail.com>
2019-12-07 14:10:56 +0100

    Add README

---
+++ modules/html-analysis/src/README.md
@@ -0,0 +1,60 @@
+# HTML analysis module
+
+This is a program for analysing HTML content regarding some features, which may indicate that site has signs of a phishing site.
+
+## Installation
+
+This application requires Node.JS (we are using currently stable version v12.10.0, but there shouldn't be any problem with a newer versions). For easier reproduction of the environment, we are using [nvm](https://github.com/nvm-sh/nvm):
+
+    # Install nvm for the current user
+    $ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
+    $ source ~/.bashrc
+    $ nvm install v12.10.0
+    $ node --version
+    v12.10.0
+
+    # Install packages for the application
+    $ npm install
+    $ node src/index.js --help
+
+There are also unit tests for testing that we are computing right values for the features. We can run them with the following command:
+
+    $ npm test
+
+## Usage
+
+The application can be used in two ways:
+
+### Command line utility mode
+
+Command line utility accepts input URL either from command line argument or standard input. We also can specify 2 output options: output as a comma separated string (used for `.csv`) or as a JSON. Example:
+
+```bash
+$ node src/index.js --url http://google.com --feat-src-link --output-values-string --include-header
+src_link,ahref_link
+0,0.5
+```
+
+### Server mode
+
+Application can also listens on the socket for input url in the JSON format
+
+    $ node src/index.js --server 12000 &  # start server on the background
+    $ telnet localhost 12000              # open the client to test the server
+    >>>
+    { "url": "http://google.com", "features": { "featSrcLink": true, "featAhrefLink": true, "featFaviconLink": true }}
+    <<<
+    {"src_link":0,"ahref_link":0.7058823529411765,"favicon_link":0}
+
+In case of error, client can receive an error:
+
+    {
+        "error": "BAD_INPUT",
+        "message": "Unexpected end of JSON input"
+    }
+We
+{    "url": "http://google.com",    "features": {        "featSrcLink": true,        "featAhrefLink": true,        "featFaviconLink": true    }}
+
+## Usage
+
+All possible options and combinations are provided in the `--help`.
--- modules/html-analysis/src/index.js
+++ modules/html-analysis/src/index.js
@@ -66,7 +66,7 @@ const checkUrl = async (url, features, argv, verbose) => {
   }
 }
 
-const getCmdlineArgs = () => {
+const parseCmdline = () => {
   // These strings are used in the specific order in option arguments. Move with a caution!
   const featureStrings = [
     'feat-input-tag', 'feat-src-link', 'feat-form-handler', 'feat-invisible-iframe', 'feat-rewrite-statusbar',
@@ -74,41 +74,37 @@ const getCmdlineArgs = () => {
     'feat-missleading-link', 'feat-hostname-title'
   ]
   const args = yargs
-    .usage('Application for phishing defence\nUsage:\n$0 [OPTION...]')
+    .usage('HTML features analysis\nUsage:\n$0 [OPTION...]')
     .help('help').alias('help', 'h')
-    .describe('verbose', 'Flag wether enable verbose mode')
-    .option('service', {
-      alias: 's',
-      type: 'boolean',
-      describe: 'start as service which is listening for '
-    }).option('port', {
-      alias: 'p',
+    .describe('verbose', 'Flag whether enable verbose mode')
+    .option('server', {
       type: 'number',
-      describe: 'port to bind on'
-    }).group(['url', 'stdin'], 'Input:')
+      describe: 'listen on socket for JSONs on a given port'
+    }).group(['url', 'stdin', 'server'], 'Input:')
     .describe('url', 'Enter one URL as parameter')
     .describe('stdin', 'Take input URLs from stdin')
-    .group(['output-json', 'output-lines', 'output-values-string', 'print-values'], 'Output formats:')
-    .describe('output-json', 'Return results as escaped JSON')
-    .describe('output-values-string', 'Return results as one string in format "<value1>,..,<value2>"')
-    .describe('output-lines', 'Return results as lines in format "<column name> <value>\\n"')
-    .describe('include-values', 'Prints also values used for tuning')
-    .describe('include-url', 'Prints also source URL')
-    .describe('include-header', 'Prints also header for csv')
-    .describe('print-only-header', 'Prints CSV header and exits')
+    .group(['output-json', 'output-lines', 'output-values-string', 'print-only-header'], 'Output formats:')
+    .describe('output-json', 'prints result as JSON')
+    .describe('output-lines', 'prints results as lines in format "COLUMN VALUE"')
+    .describe('output-values-string', 'prints results as comma separated string (suited for .csv)')
+    .describe('print-only-header', 'prints CSV header and exits')
+    .group(['include-values', 'include-url', 'include-header'], 'Extra informations in output:')
+    .describe('include-values', 'include also extra intermediate values')
+    .describe('include-url', 'include to the output given URL')
+    .describe('include-header', 'include also header line for the csv format')
     .group(featureStrings, 'Features:')
-    .describe(featureStrings[0], 'Flag wether check how many input tags has page')
-    .describe(featureStrings[1], 'Flag wether check if src=<link> is matching hostname')
-    .describe(featureStrings[2], 'Flag wether check <form> handlers')
-    .describe(featureStrings[3], 'Flag wether check invisible <iframe> elements')
-    .describe(featureStrings[4], 'Flag wether check rewriting a status bar')
-    .describe(featureStrings[5], 'Flag wether check that page has disabled right-click')
-    .describe(featureStrings[6], 'Flag wether check <a href="LINK"> pointing to outer world')
-    .describe(featureStrings[7], 'Flag wether check PopUp windows')
-    .describe(featureStrings[8], 'Flag wether check if favicon is pointing to another site')
-    .describe(featureStrings[9], 'Flag wether check if site is not running new technologies')
-    .describe(featureStrings[10], 'Flag wether check if text value of <a> link is same as actual link')
-    .describe(featureStrings[11], 'Flag wether check if hostname is matching title')
+    .describe(featureStrings[0], 'check how many input tags has page')
+    .describe(featureStrings[1], 'check if src=<link> is matching hostname')
+    .describe(featureStrings[2], 'check <form> handlers')
+    .describe(featureStrings[3], 'check invisible <iframe> elements')
+    .describe(featureStrings[4], 'check rewriting a status bar')
+    .describe(featureStrings[5], 'check that page has disabled right-click')
+    .describe(featureStrings[6], 'check <a href="LINK"> pointing to outer world')
+    .describe(featureStrings[7], 'check PopUp windows')
+    .describe(featureStrings[8], 'check if favicon is pointing to another site')
+    .describe(featureStrings[9], 'check if site is not running new technologies')
+    .describe(featureStrings[10], 'check if text value of <a> link is same as actual link')
+    .describe(featureStrings[11], 'check if hostname is matching title')
   return args
 }
 
@@ -147,7 +143,7 @@ const parseFeatures = argv => {
 }
 
 const main = async () => {
-  const argv = getCmdlineArgs()
+  const argv = parseCmdline()
   const verbose = argv.argv.verbose
 
   if (argv.argv.service) {
